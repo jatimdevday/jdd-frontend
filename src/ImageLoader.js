277ad48abf085330a706dc41f8de4435ae72e788
Fahmi Syaifudin <fahmi@ekan.id>
2024-08-28 17:37:46 +0700

    Fix: Dynamic baseurl when use imageloader

--- src/ImageLoader.js
+++ src/ImageLoader.js
@@ -1,3 +1,4 @@
+const BASE_URL = process.env["BASE_URL"] ?? "https://jdd2024-b625b.web.app";
 export default function myImageLoader({ src, width, quality }) {
-  return `https://jdd2024-b625b.web.app/${src}?w=${width}&q=${quality || 75}`;
+  return `${BASE_URL}/${src}?w=${width}&q=${quality || 75}`;
 }
