17ba39c0fd53b8c2ba634ac28ea5ab909f09fef7
Shafa Naura Priesha Ardhynindya <shafanaura48@gmail.com>
2024-08-29 20:15:35 +0700

    refactor: event section

--- src/screens/landing/sections/Event.tsx
+++ src/screens/landing/sections/Event.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
-import React from "react";
-
+import DarkStar from "@/assets/svgs/DarkStar";
 const events = [
   {
     title: "Unleashing the Power of Frontend & FullStack Web Dev",
@@ -21,57 +19,35 @@ const events = [
 
 const Event = () => {
   return (
-    <div className="bg-white text-black pt-[96px] pb-[100px]">
-      <div className="container mx-auto flex items-center">
-        <div className="w-[100%]">
-          <div className="flex mb-3 justify-center">
-            <div className="flex">
-              <Image
-                quality={100}
-                src="/black-star-2.png"
-                alt=""
-                width={20}
-                height={0}
-              />
-              <h3 className="ms-[10px] font-semibold text-[15px] me-[10px]">
-                Coming Soon
-              </h3>
-              <Image
-                quality={100}
-                src="/black-star-2.png"
-                alt=""
-                width={20}
-                height={0}
-              />
+    <div className="container mx-auto py-24">
+      <div className="flex items-center mb-1 justify-center gap-3">
+        <DarkStar />
+        <h3 className="font-semibold text-[15px]">Coming Soon</h3>
+        <DarkStar />
+      </div>
+      <h1 className="text-center mb-10 font-semibold text-[47px]">
+        Upcoming Event
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+        {events.map((event, index) => (
+          <div
+            key={index}
+            className="bg-[#F4F5FF] rounded-[15px] p-6 font-semibold flex flex-col h-full"
+          >
+            <p className="mb-6 flex-grow text-2xl">{event.title}</p>
+            <p className="font-normal mb-4">{event.speaker}</p>
+            <div className="flex flex-wrap mt-auto">
+              {event.tags.map((tag, tagIndex) => (
+                <p
+                  key={tagIndex}
+                  className="text-[12px] px-3 py-1 me-2 mb-2 rounded-[5px] bg-[#CAF93A] font-medium"
+                >
+                  {tag}
+                </p>
+              ))}
             </div>
           </div>
-          <h1 className="text-center ms-[10px] mb-10 font-semibold text-[47px] me-[10px]">
-            Upcoming Event
-          </h1>
-          <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-            {events.map((event, index) => (
-              <div
-                key={index}
-                className="bg-[#F4F5FF] rounded-[15px] p-6 font-semibold text-[24px] flex flex-col h-full"
-              >
-                <h2 className="mb-6 flex-grow">{event.title}</h2>
-                <h5 className="font-normal text-[18px] mb-4">
-                  {event.speaker}
-                </h5>
-                <div className="flex flex-wrap mt-auto">
-                  {event.tags.map((tag, tagIndex) => (
-                    <p
-                      key={tagIndex}
-                      className="text-[12px] px-3 py-1 me-2 mb-2 rounded-[5px] bg-[#CAF93A] font-medium"
-                    >
-                      {tag}
-                    </p>
-                  ))}
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
