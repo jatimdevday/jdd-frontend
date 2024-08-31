06a9d525b227c108c330f034e604b6ea4bfc4482
iqbalmnaufal <iqbalmnaufal@gmail.com>
2024-09-01 00:59:58 +0700

    feat(speaker): picture side

--- src/screens/landing/sections/Speaker.tsx
+++ src/screens/landing/sections/Speaker.tsx
@@ -1,8 +1,32 @@
+import Image from "next/image";
+
 const Speaker = () => {
   return (
     <div className="bg-primary text-white pt-[96px] pb-[170px]">
       <div className="container mx-auto">
-        Hello World
+        <Image
+          src="/speaker-example.png"
+          alt="Speakers"
+          width={583}
+          height={657}
+        />
+
+        <div className="flex items-center justify-center gap-4 mt-4 mr-[200px]">
+          <Image
+            src="/left-arrow.png"
+            alt="Arrow Left"
+            width={40}
+            height={40}
+            className="cursor-pointer"
+          />
+          <Image
+            src="/right-arrow.png"
+            alt="Arrow Right"
+            width={40}
+            height={40}
+            className="cursor-pointer"
+          />
+        </div>
       </div>
     </div>
   );
