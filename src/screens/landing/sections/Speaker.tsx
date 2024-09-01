370b9210a85e2b6f09d92b52a6ca8c46612812dc
iqbalmnaufal <iqbalmnaufal@gmail.com>
2024-09-01 11:30:15 +0700

    feat(speaker): add carousel

--- src/screens/landing/sections/Speaker.tsx
+++ src/screens/landing/sections/Speaker.tsx
@@ -1,35 +1,82 @@
+"use client"
+
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 import DarkStar from "@/assets/svgs/DarkStar";
 
 const Speaker = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isTransitioning, setIsTransitioning] = useState(false);
+
+  const images = [
+    {
+      src: "/speaker-example.png",
+      alt: "Speaker Example",
+    },
+    // TODO: just for testing image transition, remove later and add more images
+    // {
+    //   src: "/character-angry.png",
+    //   alt: "Character Angry",
+    // },
+  ];
+
+  const goToPrevious = () => {
+    setIsTransitioning(true);
+    setTimeout(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === 0 ? images.length - 1 : prevIndex - 1
+      );
+      setIsTransitioning(false);
+    }, 200); // Half of the transition duration
+  };
+
+  const goToNext = () => {
+    setIsTransitioning(true);
+    setTimeout(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+      setIsTransitioning(false);
+    }, 200); // Half of the transition duration
+  };
+
   return (
     <div className="bg-primary text-white pt-[96px] pb-[170px]">
       <div className="grid grid-cols-2 container mx-auto py-10 items-center justify-between gap-[100px]">
         {/* left side */}
-        <div>
+        <div
+          style={{
+            position: 'relative',
+            opacity: isTransitioning ? 0 : 1,
+            transition: 'opacity 0.4s ease-in-out',
+          }}
+        >
           <Image
-            src="/speaker-example.png"
-            alt="Speakers"
+            src={images[currentIndex].src}
+            alt={images[currentIndex].alt}
             width={583}
             height={657}
           />
 
           <div className="flex items-center justify-end gap-4 mt-4 mr-4">
+            {/* TODO: should this using svg? */}
             <Image
               src="/left-arrow.png"
               alt="Arrow Left"
               width={40}
               height={40}
-              className="cursor-pointer"
+              className={`cursor-pointer ${isTransitioning ? 'opacity-50' : ''}`}
+              onClick={() => !isTransitioning && goToPrevious()}
             />
             <Image
               src="/right-arrow.png"
               alt="Arrow Right"
               width={40}
               height={40}
-              className="cursor-pointer"
+              className={`cursor-pointer ${isTransitioning ? 'opacity-50' : ''}`}
+              onClick={() => !isTransitioning && goToNext()}
             />
           </div>
         </div>
