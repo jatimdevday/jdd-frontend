7ea0bbff2f08bae8f8e3bc4b302f4b8a9d09af16
iqbalmnaufal <iqbalmnaufal@gmail.com>
2024-09-02 21:16:02 +0700

    feat(agenda): add 2019 section

--- src/screens/landing/sections/Agenda.tsx
+++ src/screens/landing/sections/Agenda.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 import Star from "@/assets/svgs/Star";
 
 const Agenda = () => {
@@ -9,7 +11,46 @@ const Agenda = () => {
         <Star />
       </div>
 
-      <p className="mt-20 text-center text-2xl font-bold">Jatim Developer Day 2019</p>
+      <div className="grid grid-cols-[1fr_10px_1fr] grid-rows-[auto_1fr_auto_1fr] gap-5">
+        <p className="mt-20 text-center col-span-full text-2xl font-bold">Jatim Developer Day 2019</p>
+
+        {/* TODO: is this scrollbar? or just a line? */}
+        <div className="col-start-2 w-[10px] bg-[#1a1a4a] relative cursor-pointer">
+          <div className="w-full bg-[#8a4fff] absolute rounded-[5px]"></div>
+        </div>
+
+        <div className="">
+          <div>Universitas Ciputra, Surabaya</div>
+          <div className="mt-4 overflow-hidden">
+            <div className="grid grid-cols-2 gap-3 max-w-[520px] transition ease-in-out duration-300 transform">
+              <Image
+                src="/image-1.png"
+                alt="JDD 2019"
+                width={250}
+                height={200}
+              />
+              <Image
+                src="/image-2.png"
+                alt="JDD 2019"
+                width={250}
+                height={200}
+              />
+              <Image
+                src="/image-3.png"
+                alt="JDD 2019"
+                width={250}
+                height={200}
+              />
+              <Image
+                src="/image-4.png"
+                alt="JDD 2019"
+                width={250}
+                height={200}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
