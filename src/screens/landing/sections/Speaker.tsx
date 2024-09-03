8ef7a6ecf93639405325261848554f39216f0358
Shafa Naura Priesha Ardhynindya <shafanaura48@gmail.com>
2024-09-03 15:00:06 +0700

    restyling

--- src/screens/landing/sections/Speaker.tsx
+++ src/screens/landing/sections/Speaker.tsx
@@ -1,10 +1,26 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
 import DarkStar from "@/assets/svgs/DarkStar";
+import Arrow from "@/assets/svgs/Arrow";
+
+const WrapperLink = ({
+  label,
+  link = "#",
+}: {
+  label: string;
+  link: string;
+}) => (
+  <div className="flex items-center gap-3">
+    <DarkStar />
+    <Link href={link} className="text-base" target="_blank">
+      {label}
+    </Link>
+  </div>
+);
 
 const Speaker = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -16,10 +32,10 @@ const Speaker = () => {
       alt: "Speaker Example",
     },
     // TODO: just for testing image transition, remove later and add more images
-    // {
-    //   src: "/character-angry.png",
-    //   alt: "Character Angry",
-    // },
+    {
+      src: "/character-angry.png",
+      alt: "Character Angry",
+    },
   ];
 
   const goToPrevious = () => {
@@ -43,70 +59,61 @@ const Speaker = () => {
   };
 
   return (
-    <div className="bg-primary text-white pt-[96px] pb-[170px]">
+    <div className="bg-primary py-[120px]">
       <div className="grid grid-cols-2 container mx-auto py-10 items-center justify-between gap-[100px]">
         {/* left side */}
-        <div
-          style={{
-            position: 'relative',
-            opacity: isTransitioning ? 0 : 1,
-            transition: 'opacity 0.4s ease-in-out',
-          }}
-        >
+        <div>
           <Image
+            quality={100}
             src={images[currentIndex].src}
             alt={images[currentIndex].alt}
             width={583}
             height={657}
+            style={{
+              position: "relative",
+              opacity: isTransitioning ? 0 : 1,
+              transition: "opacity 0.4s ease-in-out",
+            }}
           />
 
-          <div className="flex items-center justify-end gap-4 mt-4 mr-4">
-            {/* TODO: should this using svg? */}
-            <Image
-              src="/left-arrow.png"
-              alt="Arrow Left"
-              width={40}
-              height={40}
-              className={`cursor-pointer ${isTransitioning ? 'opacity-50' : ''}`}
+          <div className="flex items-center justify-end gap-4 mt-8">
+            <Arrow
+              className="cursor-pointer rotate-180"
               onClick={() => !isTransitioning && goToPrevious()}
             />
-            <Image
-              src="/right-arrow.png"
-              alt="Arrow Right"
-              width={40}
-              height={40}
-              className={`cursor-pointer ${isTransitioning ? 'opacity-50' : ''}`}
+            <Arrow
+              className="cursor-pointer"
               onClick={() => !isTransitioning && goToNext()}
             />
           </div>
         </div>
 
         {/* right side */}
-        <div className="flex flex-col gap-[50px] text-[#212121]">
+        <div>
           <div className="flex mb-10 items-center gap-3">
             <DarkStar />
-            <p className="font-semibold text-lg">Meets Our Speakers</p>
+            <p className="font-semibold text-lg">Meet Our Speakers</p>
             <DarkStar />
           </div>
-          <p className="text-5xl font-semibold">
-            Amirul Ihsan
-          </p>
-          <p className="text-2xl font-semibold">
-            Lorem ipsum dolor sit amet consectetur. Lectus orci in adipiscing metus tellus faucibus id odio interdum. Bibendum aliquam et imperdiet enim. Fermentum est nullam ac odio integer. Ullamcorper morbi lacus ornare imperdiet augue vel volutpat.
-            Potenti hendrerit nulla at lectus sit integer. Erat sed tellus quis nec et consectetur gravida arcu. Duis nisi semper nunc fusce feugiat pharetra risus id. Risus.
+          <p className="text-5xl font-semibold mb-6">Amirul Ihsan</p>
+          <p className="text-xl font-semibold leading-relaxed">
+            Lorem ipsum dolor sit amet consectetur. Lectus orci in adipiscing
+            metus tellus faucibus id odio interdum. Bibendum aliquam et
+            imperdiet enim. Fermentum est nullam ac odio integer. Ullamcorper
+            morbi lacus ornare imperdiet augue vel volutpat. Potenti hendrerit
+            nulla at lectus sit integer. Erat sed tellus quis nec et consectetur
+            gravida arcu. Duis nisi semper nunc fusce feugiat pharetra risus id.
+            Risus.
           </p>
 
-          <hr className="border-black font-bold" />
+          <hr className="border-blackText my-14" />
 
-          <div className="flex items-center gap-3">
-            <DarkStar />
-            <Link href="/" className="text-base">Instagram</Link>
+          <div className="flex items-center gap-4">
+            <WrapperLink label="LinkedIn" link="#" />
             <div>|</div>
-            <DarkStar />
-            <Link href="/" className="text-base">Youtube</Link>
+            <WrapperLink label="Youtube" link="#" />
             <div>|</div>
-            <DarkStar />
-            <Link href="/" className="text-base">Instagram</Link>
+            <WrapperLink label="Instagram" link="#" />
           </div>
         </div>
       </div>
