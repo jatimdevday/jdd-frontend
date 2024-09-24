cfd99dbed8be71aa1d93d3309b04dc2f1d9eacf3
Shafa Naura Priesha Ardhynindya <shafanaura48@gmail.com>
2024-09-24 22:07:00 +0700

    fix: style minor

--- src/screens/landing/sections/Speaker.tsx
+++ src/screens/landing/sections/Speaker.tsx
@@ -4,18 +4,19 @@ import { Speakers } from "@/lib/schema";
 import { getSpeakers } from "@/lib/firebase";
 
 const Speaker = async () => {
-
   const speakers = (await getSpeakers()) as Speakers;
 
   return (
-    <div className="bg-primary py-16 md:py-24" id="speakers">
-      <div className="container mx-auto py-10 items-center justify-between">
-        <div className="flex mb-6 md:mb-10 items-center gap-3">
+    <div className="bg-primary py-12 md:py-24" id="speakers">
+      <div className="container mx-auto items-center justify-between">
+        <div className="flex mb-6 items-center gap-3 justify-center md:justify-start">
           <DarkStar />
           <p className="font-semibold md:text-lg">Meet Our Speakers</p>
           <DarkStar />
         </div>
-        <div className="text-2xl font-bold">With The Feature Speaker</div>
+        <div className="text-2xl font-bold text-center md:text-start">
+          With The Feature Speaker
+        </div>
 
         {/* TODO: maybe need to change into justify-around and so on */}
         <div className="flex flex-col lg:flex-row justify-between items-center lg:mt-12">
@@ -29,8 +30,12 @@ const Speaker = async () => {
                 width={400}
                 height={400}
               />
-              <div className="mt-6 font-semibold text-base">{speaker.title}</div>
-              <div className="mt-4 font-semibold text-4xl">{speaker.name}</div>
+              <div className="mt-6 font-semibold text-base">
+                {speaker.title}
+              </div>
+              <div className="mt-4 font-semibold text-3xl md:text-4xl">
+                {speaker.name}
+              </div>
             </div>
           ))}
         </div>
