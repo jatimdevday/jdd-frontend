ffeb76ab7cd714aeb2281d14732285ac4b1f7917
Nusendra Hanggarawan <admin@nusendra.com>
2024-09-26 14:10:48 +0700

    fix: cta ui improvements

--- src/screens/landing/sections/Cta.tsx
+++ src/screens/landing/sections/Cta.tsx
@@ -1,36 +1,32 @@
-import DarkStar from "@/assets/svgs/DarkStar";
+import Star from "@/assets/svgs/Star";
+import Star3 from "@/assets/svgs/Star3";
+import World2 from "@/assets/svgs/World2";
+import Click from "@/assets/svgs/Click";
 import { Button } from "@/components/Button";
 import { Content } from "@/lib/schema";
-import Image from "next/image";
 import React from "react";
 
 const Cta = ({ content }: { content?: Content }) => {
   return (
-    <div className="py-16 md:py-24">
-      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
-        <div className="w-[100%] md:w-[63%] text-center md:text-left mb-6 md:mb-0">
-          <div className="flex items-center gap-3 justify-center md:justify-start">
-            <DarkStar />
-            <p className="font-semibold">Main Event</p>
-            <DarkStar />
+    <div className="flex justify-center bg-secondary">
+      <Star3 />
+      <div className="relative container mx-auto text-center py-16 md:py-24">
+        <div className="mb-6">
+          <div className="flex items-center gap-3 justify-center">
+            <Star />
+            <p className="font-semibold text-white">Main Event</p>
+            <Star />
           </div>
-          <p className="font-semibold text-3xl md:text-[32px] leading-snug mb-6 mt-4">
+          <p className="font-semibold text-3xl md:text-[32px] leading-snug mb-12 mt-4 text-white">
             {content?.registration_tagline}
           </p>
           <a href={content?.registration_link}>
-            <Button variant="secondary">Daftar Sekarang</Button>
+            <Button variant="default">
+              <span className="mr-2">Daftar Sekarang</span> <Click />
+            </Button>
           </a>
         </div>
-        <div className="w-[100%] md:w-[30%] hidden md:block">
-          <Image
-            quality={100}
-            src="/character-angry.png"
-            alt=""
-            layout="responsive"
-            width={300}
-            height={200}
-          />
-        </div>
+        <World2 className="absolute bottom-0 right-0" />
       </div>
     </div>
   );
